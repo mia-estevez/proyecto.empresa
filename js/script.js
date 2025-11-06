4860a076e9a02939cdc471f8c270c1bf9ee24a07
Agustina-Aros <agustinaaros38@gmail.com>
2025-11-06 03:29:44 -0300

    Creando la funcion del click izquierdo- js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,12 @@ boton.addEventListener('click', () => {
 
 });
 
-const boton = document.getElementById('botonEnviar');
+const botón = document.getElementById('botonEnviar');
 const modalConfirmar = document.getElementById('modalConfirmar');
 const modalLimpiar = document.getElementById('modalLimpiar');
 const formulario = document.getElementById('miFormulario');
+
+boton.addEventListener('click', (e) => {
+  e.preventDefault(); // Evita enviar directamente
+  modalConfirmar.style.display = 'block';
+});
