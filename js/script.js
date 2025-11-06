fc644a76b2945c0a4992d5eb98839923b7f35b3d
Agustina-Aros <agustinaaros38@gmail.com>
2025-11-06 02:55:17 -0300

    Agregando texto al boton modo noche- js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 const boton= document.getElementById('modo-btn');
 boton.addEventListener('click', () => {
     document.body.classList.toggle('modo-noche');
+
+    if (document.body.classList.contains('modo-noche')) {
+        boton.textContent = 'Desactivar modo noche';
+    } else {
+        boton.textContent = 'Activar modo noche';
+    }
+
 });
